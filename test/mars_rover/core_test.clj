88961d7e60ebbce7db248cd3c2ededc8c1a72f5c
trikitrok <trikitrok@gmail.com>
2015-03-03 19:35:15 +0100

    Rearranged tests

--- test/mars_rover/core_test.clj
+++ test/mars_rover/core_test.clj
@@ -7,61 +7,64 @@
   "about mars rover"
   
   (facts 
-    "when facing north"
+    "about rotations"
     
-    (fact 
-      "it turns right"
-      (receive 
-        (rover 0 0 :north)
-        "r") => (rover 0 0 :east))
+    (facts 
+      "when facing north"
+      
+      (fact 
+        "it turns right"
+        (receive 
+          (rover 0 0 :north)
+          "r") => (rover 0 0 :east))
+      
+      (fact 
+        "it turns left"
+        (receive 
+          (rover 0 0 :north) 
+          "l") => (rover 0 0 :west)))
     
-    (fact 
-      "it turns left"
-      (receive 
-        (rover 0 0 :north) 
-        "l") => (rover 0 0 :west)))
-  
-  (facts
-    "when facing south"
+    (facts
+      "when facing south"
+      
+      (fact 
+        "it turns right"
+        (receive
+          (rover 0 0 :south)
+          "r") => (rover 0 0 :west))
+      
+      (fact 
+        "it turns left"
+        (receive
+          (rover 0 0 :south)
+          "l") => (rover 0 0 :east)))
     
-    (fact 
-      "it turns right"
-      (receive
-        (rover 0 0 :south)
-        "r") => (rover 0 0 :west))
+    (facts
+      "when facing east"
+      
+      (fact 
+        "it turns right"
+        (receive
+          (rover 0 0 :east)
+          "r") => (rover 0 0 :south))
+      
+      (fact 
+        "it turns left"
+        (receive
+          (rover 0 0 :east)
+          "l") => (rover 0 0 :north)))
     
-    (fact 
-      "it turns left"
-      (receive
-        (rover 0 0 :south)
-        "l") => (rover 0 0 :east)))
-  
-  (facts
-    "when facing east"
-    
-    (fact 
-      "it turns right"
-      (receive
-        (rover 0 0 :east)
-        "r") => (rover 0 0 :south))
-    
-    (fact 
-      "it turns left"
-      (receive
-        (rover 0 0 :east)
-        "l") => (rover 0 0 :north)))
-  
-  (facts 
-    "when facing west"
-    
-    (fact 
-      "it turns right"
-      (receive
-        (rover 0 0 :west)
-        "r") => (rover 0 0 :north))
-    
-    (fact 
-      "it turns left"
-      (receive
-        (rover 0 0 :west)
-        "l") => (rover 0 0 :south))))
+    (facts 
+      "when facing west"
+      
+      (fact 
+        "it turns right"
+        (receive
+          (rover 0 0 :west)
+          "r") => (rover 0 0 :north))
+      
+      (fact 
+        "it turns left"
+        (receive
+          (rover 0 0 :west)
+          "l") => (rover 0 0 :south)))))
