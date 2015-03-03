f118de07195e033137d220cdfc6701d7f359d011
trikitrok <trikitrok@gmail.com>
2015-03-03 19:23:59 +0100

    Reformatted code

--- src/mars_rover/core.clj
+++ src/mars_rover/core.clj
@@ -6,16 +6,13 @@
 
 (defn receive [{x :x y :y direction :direction} commands]
   
-  (cond (= :north direction)
-        (if (= commands "l")
-          (rover 0 0 :west)
-          (rover 0 0 :east))
+  (cond (= :north direction) (if (= commands "l")
+                               (rover 0 0 :west)
+                               (rover 0 0 :east))
         
-        (= :south direction)
+        (= :south direction) (if (= commands "l")
+                               (rover 0 0 :east)
+                               (rover 0 0 :west))
         
-        (if (= commands "l")
-          (rover 0 0 :east)
-          (rover 0 0 :west))
-        :else
-        (rover 0 0 :south)))
+        :else (rover 0 0 :south)))
 
