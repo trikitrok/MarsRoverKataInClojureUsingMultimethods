bb7d8c6c4105f4a7743bcfa1e2caff7818ce1490
trikitrok <trikitrok@gmail.com>
2015-03-08 17:20:01 +0100

    Reformatted code

--- src/mars_rover/worlds.clj
+++ src/mars_rover/worlds.clj
@@ -1,25 +1,26 @@
 (ns mars-rover.worlds)
 
 (defn square-world [x y size & obstacles]
-  {:wrap-fn (fn [{x-rover :x y-rover :y :as rover}] 
-              (cond 
-                (and (> y-rover y) 
-                     (> (- y-rover y) size)) 
-                (assoc-in rover [:y] (- y-rover size))
-                
-                (and (< y-rover y) 
-                     (< (- y y-rover) size)) 
-                (assoc-in rover [:y] (+ y-rover size))
-                
-                (and (> x-rover x) 
-                     (> (- x-rover x) size)) 
-                (assoc-in rover [:x] (- x-rover size))
-                
-                (and (< x-rover x) 
-                     (< (- x x-rover) size)) 
-                (assoc-in rover [:x] (+ x-rover size))
-                
-                :else rover))
+  {:wrap-fn 
+   (fn [{x-rover :x y-rover :y :as rover}] 
+     (cond 
+       (and (> y-rover y) 
+            (> (- y-rover y) size)) 
+       (assoc-in rover [:y] (- y-rover size))
+       
+       (and (< y-rover y) 
+            (< (- y y-rover) size)) 
+       (assoc-in rover [:y] (+ y-rover size))
+       
+       (and (> x-rover x) 
+            (> (- x-rover x) size)) 
+       (assoc-in rover [:x] (- x-rover size))
+       
+       (and (< x-rover x) 
+            (< (- x x-rover) size)) 
+       (assoc-in rover [:x] (+ x-rover size))
+       
+       :else rover))
    
    :obstacles obstacles})
 
@@ -29,4 +30,5 @@
    :obstacles []})
 
 (defn hit-obstacle? [{x-rover :x y-rover :y} obstacles]
-  (= (some #{{:x x-rover :y y-rover}} obstacles) {:x x-rover :y y-rover}))
+  (= (some #{{:x x-rover :y y-rover}} obstacles) 
+     {:x x-rover :y y-rover}))
